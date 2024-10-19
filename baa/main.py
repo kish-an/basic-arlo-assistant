8c6ee8c47ffcbd96570aec837f48e37b643e6283
Kishan <kishan.o@hotmail.com>
2024-10-19 20:37:11 +0100

    Add app entry point

---
+++ baa/main.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from typing import Optional
+from attendee_parser import butter
+
+
+def baa(
+    attendee_file: Path,
+    format: str,
+    platform: str,
+    course_code: Optional[str],
+    username: str,
+    password: str,
+) -> None:
+    meeting = butter.get_attendees(attendee_file, course_code)
