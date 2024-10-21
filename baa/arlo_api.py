8341f970b1d35537b5469de0b516fe80eb0e0fd0
Kishan <kishan.o@hotmail.com>
2024-10-21 14:52:19 +0100

    Get Arlo session registrations

--- baa/arlo_api.py
+++ baa/arlo_api.py
@@ -17,6 +17,7 @@ class ArloClient:
         self.session = requests.Session()
         self.session.auth = HTTPBasicAuth(*get_keyring_credentials())
         self.base_url = f"https://{platform}.arlo.co/api/2012-02-01/auth/resources"
+        self.missing_students = []
 
     def _get_response(self, url: str, params: dict = None) -> requests.Response:
         res = self.session.get(url, params=params)
@@ -45,9 +46,7 @@ class ArloClient:
         return root
 
     def _get_event_id(self, event_code: str) -> str:
-        res = self._get_response(
-            f"{self.base_url}/events", params={"expand": "Event"}
-        )
+        res = self._get_response(f"{self.base_url}/events", params={"expand": "Event"})
 
         event_tree = self._append_paginated(root=etree.fromstring(res.content))
         event_id = event_tree.findtext(f".//Code[. ='{event_code}']/../EventID")
@@ -60,7 +59,8 @@ class ArloClient:
 
     def _get_session_id(self, event_id: str, start_date: datetime) -> str:
         res = self._get_response(
-            f"{self.base_url}/events/{event_id}/sessions", params={"expand": "EventSession"}
+            f"{self.base_url}/events/{event_id}/sessions",
+            params={"expand": "EventSession"},
         )
 
         date = start_date.strftime("%Y-%m-%d")
@@ -73,3 +73,25 @@ class ArloClient:
             raise SessionNotFound(f"🚨 No session found on: {date}")
 
         return session_ids[0]
+
+    def _get_session_registrations(self, session_id: str) -> etree._Element:
+        res = self._get_response(
+            f"{self.base_url}/eventsessions/{session_id}/registrations",
+            params={
+                "expand": "EventSessionRegistration,EventSessionRegistration/ParentRegistration,EventSessionRegistration/ParentRegistration/Contact"
+            },
+        )
+
+        reg_tree = self._append_paginated(root=etree.fromstring(res.content))
+
+        return reg_tree
+
+    def get_registrations(
+        self, event_code: str, session_date: datetime
+    ) -> etree._Element:
+        event_id = self._get_event_id(event_code)
+        session_id = self._get_session_id(event_id, session_date)
+        return self._get_session_registrations(session_id)
+
+    def update_attendance(session_reg_href: str) -> bool:
+        pass
--- baa/main.py
+++ baa/main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing import Optional
 
 from baa.attendee_parser import butter
+from baa.arlo_api import ArloClient
 
 
 def baa(
@@ -15,3 +16,8 @@ def baa(
 ) -> None:
     meeting = butter.get_attendees(attendee_file, event_code)
     meeting.attendees.sort()
+
+    arlo_client = ArloClient(platform)
+    registrations = arlo_client.get_registrations(
+        event_code or meeting.event_code, date or meeting.start_date
+    )
