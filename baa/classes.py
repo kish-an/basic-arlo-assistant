0a8a20afb7937c87304106cb9e85a26f227e8ad2
Kishan <kishan.o@hotmail.com>
2024-10-27 23:00:25 +0000

    Update docstrings for classes

--- baa/classes.py
+++ baa/classes.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 @dataclass(kw_only=True)
 class Attendee(ABC):
-    """Attendee base class. All format specific attendee classes should derive from this"""
+    """Base class for all attendee types, containing common attributes. All format specific attendee classes should derive from this"""
 
     name: str
     email: str
@@ -15,11 +15,15 @@ class Attendee(ABC):
 
 @dataclass(kw_only=True)
 class ButterAttendee(Attendee):
+    """Attendee class for Butter events, including session duration."""
+
     session_duration: float
 
 
 @dataclass(kw_only=True)
 class ArloRegistration(Attendee):
+    """Attendee class for Arlo registrations with a registration link."""
+
     reg_href: str
 
     def __eq__(self, other):
@@ -37,12 +41,16 @@ class ArloRegistration(Attendee):
 
 @dataclass
 class Meeting:
+    """Represents a meeting with an event code, start date, and list of attendees."""
+
     event_code: str
     start_date: datetime
     attendees: list[Attendee]
 
 
 class AttendanceStatus(Enum):
+    """Possible attendance statuses for Arlo registration"""
+
     ATTENDED = "Attended"
     DID_NOT_ATTEND = "DidNotAttend"
     UNKNOWN = "Unknown"
